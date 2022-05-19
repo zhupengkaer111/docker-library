1b8d279ee81d9efae360e82998bee18d2d356d45
zhupeng <Unilumin123*>
2022-05-19 16:23:10 +0800

    redis 基础镜像

--- docker-redis-base/docker-entrypoint.sh
+++ docker-redis-base/docker-entrypoint.sh
@@ -9,8 +9,8 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	find . \! -user redis -exec chown redis '{}' +
-	exec gosu redis "$0" "$@"
+	find . \! -user uni-redis -exec chown redis '{}' +
+	exec gosu uni-redis "$0" "$@"
 fi
 
 exec "$@"
