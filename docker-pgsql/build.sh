9075aed53d5f166cc6cb5b6eeac2824b9d647229
zhupeng <Unilumin123*>
2022-06-14 19:35:49 +0800

    修改pgsql base Dockerfile

--- docker-pgsql/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t 10.1.50.181:5000/unismart-pgsql:13.5 . --network=host
--- docker-pgsql/reRun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker stop unismart-pgsql
-docker rm unismart-pgsql
-docker run -itd \
-	--name unismart-pgsql \
-	-p 5432:5432 \
-	-e POSTGRES_PASSWORD=Unilumin123* \
-	-e POSTGRES_USER=ulm2 \
-	unismart-pgsql:1.14.0
